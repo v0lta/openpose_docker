4a0f65d41304465158557f22dd9be5f303d8e47e
moritz <wolter@cs.uni-bonn.de>
2019-08-19 17:44:26 +0200

    torso normal runs.

--- orientation_estimation.py
+++ orientation_estimation.py
@@ -1,9 +1,11 @@
 import cv2
 import sys
+import time
 import imageio
 import ipdb
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 sys.path.append('/usr/local/python')
 import openpose.pyopenpose as op
 
@@ -29,36 +31,43 @@ vid = imageio.get_reader(filename, 'ffmpeg')
 vid_depth = imageio.get_reader(filename_depth, 'ffmpeg')
 
 
-plot = True
+plot = False
 write_movie = True
 if write_movie:
     writer = imageio.get_writer('./orientation.mp4', fps=25)
 
 for frame_no, frame in enumerate(vid):
 
+    start = time.time()
+
     depth_frame = vid_depth.get_next_data()[..., 0]
     depth_frame_norm = depth_frame/np.max(depth_frame)
+
     datum = op.Datum()
     datum.cvInputData = frame
     opWrapper.emplaceAndPop([datum])
     # print("Body keypoints: \n" + str(datum.poseKeypoints))
     # cv2.imshow("OpenPose 1.5.0 - Tutorial Python API", datum.cvOutputData)
-    print(frame_no)
-    if frame_no > 25:
 
-        plt.imshow(frame)
-        plt.savefig('frame')
+    stop = time.time()
+    print('frame', frame_no, 'processed. In:', stop-start)
+
+    if frame_no > 0:
+
+        # plt.imshow(frame)
+        # plt.savefig('frame')
 
         heatmaps = datum.poseHeatMaps.copy()
         heatmaps = (heatmaps).astype(dtype='uint8')
 
+        joint_total = 25
         ls_heat = heatmaps[2, :, :]
         rs_heat = heatmaps[5, :, :]
         rh_heat = heatmaps[9, :, :]
         lh_heat = heatmaps[12, :, :]
 
-        depth = cv2.resize(depth_frame, (heatmaps.shape[2],
-                                         heatmaps.shape[1]))
+        depth_frame_norm = cv2.resize(depth_frame_norm, (heatmaps.shape[2],
+                                                         heatmaps.shape[1]))
 
         if plot:
             outputImageF = (datum.inputNetData[0].copy())[0, :, :, :] + 0.5
@@ -68,7 +77,7 @@ for frame_no, frame in enumerate(vid):
             outputImageF = (outputImageF*255.).astype(dtype='uint8')
 
             num_maps = heatmaps.shape[0]
-            for counter in range(num_maps):
+            for counter in range(joint_total):
                 # map position - joint
                 # 2,  Left-Shoulder,
                 # 5,  Right-Shoulder,
@@ -81,8 +90,97 @@ for frame_no, frame in enumerate(vid):
                 print('counter', counter)
                 plt.imshow(combined)
                 plt.savefig('combined.png')
-                ipdb.set_trace()
+                # ipdb.set_trace()
                 counter += 1
-                counter = counter % num_maps
 
-        ipdb.set_trace()
+        # compute joint positions.]
+        joints_xyz = []
+        zero_canvas = heatmaps[0, :, :]*0.0
+        # plt.imshow(to_plot)
+        # plt.show()
+        for joint_no in range(joint_total):
+            # print(joint_no)
+            # zero_canvas = zero_canvas + heatmap[:, :, joint_no]
+            # plt.imshow(zero_canvas)
+            # plt.show()
+            nx, ny = heatmaps.shape[1:]
+            x = np.linspace(0, 1, nx)
+            y = np.linspace(0, 1, ny)
+            x_grid, y_grid = np.meshgrid(y, x)
+            y_grid = np.flipud(y_grid)
+
+            def mean_coordinate(coords, heat, thre1=0.1):
+                heat = np.where(heat > thre1, heat, 0)
+                coords = coords * heat
+                return np.sum(coords.flatten())/np.sum(heat.flatten())
+
+            mean_x = mean_coordinate(x_grid, heatmaps[joint_no, :, :])
+            mean_y = mean_coordinate(y_grid, heatmaps[joint_no, :, :])
+            mean_z = mean_coordinate(depth_frame_norm, heatmaps[joint_no, :, :])
+            joints_xyz.append((mean_x, mean_y, mean_z))
+            joints_xyz_array = np.array(joints_xyz)
+            # plt.plot(joints_xyz_array[:, 0], joints_xyz_array[:, 1], '.')
+            # plt.show()
+
+        joints_xyz = np.array(joints_xyz)
+
+        # plt.plot(joints_xyz[1:14, 0], joints_xyz[1:14, 1], '.')
+        # plt.show()
+
+        # ipdb.set_trace()
+        fig = plt.figure()
+        ax = fig.add_subplot(121)
+        ax.imshow(datum.cvOutputData)
+
+        ax = fig.add_subplot(122, projection='3d')
+
+        left_shoulder = joints_xyz[5, :]
+        right_shoulder = joints_xyz[2, :]
+        left_hip = joints_xyz[11, :]
+        right_hip = joints_xyz[8, :]
+
+        shoulder_hip_rectangle = np.array([left_hip, right_hip,
+                                           left_shoulder, right_shoulder])
+        ax.scatter(shoulder_hip_rectangle[:, 0],
+                   shoulder_hip_rectangle[:, 2],
+                   shoulder_hip_rectangle[:, 1], c='b')
+
+        # compute all 4 normal vectors for the plane.
+        lh_rh = left_hip - right_hip
+        lh_ls = left_hip - left_shoulder
+        lh_rh_ls_n = np.cross(lh_rh, lh_ls)
+
+        rh_rs = right_hip - right_shoulder
+        rh_lh = right_hip - left_hip
+        rh_lh_rs_n = np.cross(rh_rs, rh_lh)
+
+        ls_lh = left_shoulder - left_hip
+        ls_rs = left_shoulder - right_shoulder
+        ls_rh_ls_n = np.cross(ls_lh, ls_rs)
+
+        rs_ls = right_shoulder - left_shoulder
+        rs_rh = right_shoulder - right_hip
+        rs_ls_rh_n = np.cross(rs_ls, rs_rh)
+
+        center = np.mean(shoulder_hip_rectangle, axis=0)
+        normals = np.array([lh_rh_ls_n, rh_lh_rs_n, ls_rh_ls_n, rs_ls_rh_n])
+        normal = np.mean(normals, axis=0)
+        tau = 3
+        norm_vec = np.array([center, center + tau*normal])
+        ax.plot(norm_vec[:, 0],
+                norm_vec[:, 2],
+                norm_vec[:, 1], c='r')
+        ax.scatter(norm_vec[-1, 0],
+                   norm_vec[-1, 2],
+                   norm_vec[-1, 1], c='r', s=60)
+
+        if write_movie:
+            plt.axis('off')
+            fig.canvas.draw()
+            # ipdb.set_trace()
+            to_plot = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+            to_plot = to_plot.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+            writer.append_data(to_plot)
+            plt.close()
+        else:
+            plt.show()
